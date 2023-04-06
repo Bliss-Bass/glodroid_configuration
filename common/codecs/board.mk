1aba63fc59f21e0f1d83cf0ffba53db972abc84b
Michał Gapiński <mike@gapinski.eu>
2023-04-06 14:45:25 +0200

    Add v4l2_codec2(h264 encoder) and ffmpeg_codec2(hwaccel h264, h265 + various sw decoders)

--- common/codecs/board.mk
+++ common/codecs/board.mk
@@ -3,3 +3,12 @@
 # GloDroid project (https://github.com/GloDroid)
 #
 # Copyright (C) 2022 Roman Stratiienko (r.stratiienko@gmail.com)
+
+
+DEVICE_MANIFEST_FILE += \
+    glodroid/configuration/common/codecs/android.hardware.media.omx@1.0.xml \
+
+BOARD_VENDOR_SEPOLICY_DIRS += glodroid/configuration/common/codecs/sepolicy/vendor
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    glodroid/configuration/common/codecs/device_framework_compatibility_matrix_codec2.xml \
